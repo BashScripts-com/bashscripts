ec359272af10789eb771c39e0cb910a3a91bcb70
BashScripts-com <advertising@hey.com>
2025-01-09 21:40:28 -0500

    Add files via upload

---
+++ Domain_Availability_Checker.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+#	BASHSCRIPTS.com
+#
+#		Summary: Search for available .COM domains from the command line.
+#		NOTE: This script only works for .COM right now. Let us know if you want us to
+#		add more extensions. 
+#			
+#			Requirements: WHOIS (available in most repositories)
+#
+#
+#		Instructions: Provide a list of .COM domains in a separate file (one domain per line).
+#		Run the script from the terminal. It will ask you for the location of your domain
+#		list file. Enter the FULL path (i.e. /home/user/domains.txt).
+#
+#		The script will output AVAILABLE domains to the terminal (STDOUT) AND also create
+#		a results folder in the PWD (wherever you ran the script from).
+#		
+#
+#
+
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+DATE="$(date +%d-%b---%H-%M-%S)"
+
+
+RESULTS_DIRECTORY="$PWD/domain_availability_script"
+FINAL_OUTPUT="$RESULTS_DIRECTORY"/"$DATE"_final_domain_availability.txt
+
+
+read -p "enter the FULL PATH of the FILE with the domains you want to search (i.e. /home/user/domains.txt) " domain_name_list
+
+
+2>> "$RESULTS_DIRECTORY"/error_log.txt
+
+
+if [[ ! -d "$RESULTS_DIRECTORY" ]]; then
+	mkdir -m 750 -p "$RESULTS_DIRECTORY"
+fi
+
+
+
+#write info to results file
+echo ""
+echo "AVAILABLE DOMAINS:" >> $FINAL_OUTPUT
+echo "" >> $FINAL_OUTPUT
+
+#write info to STDOUT/terminal
+echo "AVAILABLE DOMAINS:"
+echo ""
+
+#using the domain list file provided, do a WHOIS lookup for each domain. Grep for language indicating the domain is available.
+#write results to file in PWD and write to STDOUT/terminal
+while read -r line;
+  do
+    (timeout 10 /usr/bin/whois -- "$line" | sed -e 's/\*//g' -e 's/\@//g' -e 's/\!//g' | tr -cd '[:alnum:]\.\- \n' | tr -s ' ' | grep -Fqi "No match for domain") && echo "${line}" >> $FINAL_OUTPUT && echo "$line"
+    sleep 2
+  done < "$domain_name_list"
+
+
+
+
