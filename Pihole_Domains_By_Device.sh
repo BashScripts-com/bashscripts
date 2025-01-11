40a093b258f8119f38e12b512524131ca9dfa7eb
BashScripts-com <advertising@hey.com>
2025-01-11 16:24:55 -0500

    Add files via upload

---
+++ Pihole_Domains_By_Device.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+#     BashScripts.com  
+#     Github: https://github.com/BashScripts-com
+#		
+#        Summary: Get all pihole DNS queries from today's log for a particular device on your network.
+#
+#			Required Programs: Pihole, SSH
+#
+#        The script will prompt you for: 
+#        			
+#        			1) the device/client ip or hostname you want DNS queries for 
+#				2) the pihole ip address (for SSH)
+#
+#		The script will SSH into your pihole, parse the pihole.log list, and display a big list DNS lookups from whichever
+#		client/device you provided. It will list the DNS queries by count in reverse descending order. 
+#	
+#
+#
+
+
+	echo -e "\n\nPIHOLE LOOKUP script -- get a device's DNS queries from today's pihole.log file"
+	sleep 2
+	echo -e "\n\nWe'll show all DNS lookups in reverse decending order (highest to lowest)\n"
+	sleep 1
+	read -p "Enter the client/device IP or hostname whose DNS lookups you want: "  device_ip
+	echo ""
+	read -p "Enter pihole SSH information (ex: user@x.x.x.x): " pihole_ip
+	sleep 2
+	echo -e "\n\nSearching DNS queries from device: "$device_ip"\n\n"
+	sleep 3
+
+
+     #use REV trick to get the 3rd column from the end
+     ssh "$pihole_ip" 'echo -e "\n\nSearching CURRENT DAY pihole logs\n\n"; sudo grep -F "query[" /var/log/pihole/pihole.log \
+	     | sudo grep -F "'$device_ip'" | rev | cut -d" " -f3 | rev | sort | uniq -c | sort -nr; echo -e "\n\n";'
+	
+
+
+
+     	sleep 3
+	echo -e "\n\nFINISHED!\n\n"
+
+
+
+
