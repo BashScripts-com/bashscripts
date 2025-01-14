1c978fde80a7a2131ee0d78d2443d66143aee906
BashScripts-com <advertising@hey.com>
2025-01-14 12:59:19 -0500

    Update Remote_Wireshark.sh
    added checks to make sure required programs are installed

--- Remote_Wireshark.sh
+++ Remote_Wireshark.sh
@@ -15,7 +15,6 @@
 #     		the remote interface to listen on, capture snaplength, and specific host to filter (optional)
 
 
-
 WIRESHARK_NAMED_PIPE="/tmp/wireshark_named_pipe"
 
 echo ""
@@ -31,20 +30,49 @@ sleep 2
 read -p "What remote interface should we capture on (eth0, eth1, etc.)? " interface
 echo ""
 sleep 2
-echo "capturing on interface "$interface""
+echo "Capturing on interface "$interface""
 echo ""
 sleep 2
 read -p "What snaplength should we use (type 0 for no limit)? " snaplength
 echo ""
 sleep 2
-echo "snaplength size is "$snaplength""
+echo "Snaplength size is "$snaplength""
 echo ""
 sleep 2
-
-
 read -p "Filter for a specific host ? (enter IP address)(just press ENTER if you don't want a specific host and want all traffic instead) " filter_host
-echo "only capture traffic for "${filter_host:-NO_HOST_ENTERED_SO_CAPTURING_ALL_HOSTS}""
+sleep 2
+echo -e "\nOnly capture traffic for "${filter_host:-NO_HOST_ENTERED_SO_CAPTURING_ALL_HOSTS}""
 echo ""
+
+
+sleep 2
+echo -e "\nChecking for required programs ....."
+sleep 2
+
+#check for required programs
+if command -v wireshark > /dev/null 2>&1; then
+	
+	echo -e "\nSUCCESS: Wireshark is installed on this device, continuing ....\n"
+
+else
+	echo -e "\nWireshark NOT FOUND. You must install it to use this script. Exiting ...\n"
+	exit
+
+fi
+
+sleep 1
+
+if ssh "$device" "command -v tcpdump"> /dev/null 2>&1; then
+	
+	echo -e "\nSUCCESS: TCPDUMP is installed on the remote device, continuing ....\n"
+
+else
+	echo -e "\nTCPDUMP NOT FOUND. You must install it on the remote device to use this script. Exiting ...\n"
+	exit
+
+fi
+
+sleep 2
 
 
 # ON LOCAL COMPUTER: if named pipe does not already exist, then create it in /tmp
@@ -75,7 +103,6 @@ ssh "$device" "/usr/sbin/tcpdump -i '"$interface"' -s'"$snaplength"' -n not port
 
 fi
 
-
 #remove named pipe
 rm "$WIRESHARK_NAMED_PIPE"
 
@@ -84,4 +111,3 @@ echo ""
 echo "SCRIPT COMPLETED -- if the script hangs, press CTRL+C to exit and return to your terminal."
 echo ""
 sleep 2
-
